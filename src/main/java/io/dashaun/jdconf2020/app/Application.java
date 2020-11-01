4e386c161c402dbf55bff10a4fca057e47047a93
DaShaun <dashaun@redislabs.com>
2020-11-01 15:19:56 -0600

    Cleanup

--- src/main/java/io/dashaun/jdconf2020/app/Application.java
+++ src/main/java/io/dashaun/jdconf2020/app/Application.java
@@ -1,15 +1,7 @@
 package io.dashaun.jdconf2020.app;
 
-import io.dashaun.jdconf2020.app.domain.PageView;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
-
 
 @SpringBootApplication
 public class Application {
@@ -19,4 +11,3 @@ public class Application {
 	}
 
 }
-
--- src/main/java/io/dashaun/jdconf2020/app/domain/PageView.java
+++ src/main/java/io/dashaun/jdconf2020/app/domain/PageView.java
@@ -2,21 +2,25 @@ package io.dashaun.jdconf2020.app.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Data
 @RedisHash("PageView")
 public class PageView implements Serializable {
+
+    public PageView(){
+        this.setCreated(LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+    }
+
     @Id
     private String id;
     private String remoteAddr;
     private String localAddr;
-    private Date created;
+    private String created;
 }
